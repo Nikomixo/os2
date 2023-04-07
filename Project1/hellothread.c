e0bb84e3eb655f8388bd4b9cc8dcd918463cad99
Nikomixo <kevint0218@outlook.com>
2023-04-07 03:26:51 -0700

    Project 1

---
+++ Project1/hellothread.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void *run(void *arg)
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        printf("%s: %d\n", (char *)arg, i);
+        sleep(0);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t t1;
+    pthread_t t2;
+
+    printf("Launching threads\n");
+
+    pthread_create(&t1, NULL, run, "thread 1");
+    pthread_create(&t2, NULL, run, "thread 2");
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Threads complete!\n");
+
+    return 0;
+}
